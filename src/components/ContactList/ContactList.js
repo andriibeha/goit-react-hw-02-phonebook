da22cb8219d60e335684f25ba398029bffcae501
andriibeha <beha.andrii@gmail.com>
2022-07-04 23:53:11 +0300

    as

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -5,37 +5,10 @@ const ContactList = ({ contact }) => {
 
     return (
         <ul>
-            {contact.length === 0 ?
-                "List is empty"
-                :
-                console.log(contact.map(item => {
-                    return item.name
-                }))
-        }
+
         </ul>
     )
 }
 
-/* class ContactList extends Component { 
-    render() { 
-        return (
-            <div>
-                <ul>
-                    {this.props.contacts.length ? 
-                        this.props.contacts.map(item => {
-                            console.log(item);
-                            <li key={item.id}>
-                                 {item.name}
-                            </li>
-                        })   :
-                        "ХУЙ"
-                    }
-                    
-                </ul>
-            </div>
-        )
-    }
-};
- */
 
 export default ContactList;
